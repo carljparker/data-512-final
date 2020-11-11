7c0a9e0fa67ee03e6e52b70d1db5b1ca88b93bd0
Carl Parker <carljparker@fb.com>
2020-11-10 20:37:34 -0800

    Get the county

--- code/get-county.py
+++ code/get-county.py
@@ -3,7 +3,7 @@ import geocoder
 
 g = geocoder.bing( 'Mountain View, CA', key=os.environ[ 'BING_API_KEY' ] )
 
-print( g.json[ 'raw' ][ '__type' ] )
+print( g.json[ 'raw' ][ 'address' ][ 'adminDistrict2' ] )
 
 
 # --- END --- #
