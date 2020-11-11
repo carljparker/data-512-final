e15ef766caf91422e897319056b628954688078d
Carl Parker <carljparker@fb.com>
2020-11-10 20:35:33 -0800

    Access nested dictionary items

--- code/get-county.py
+++ code/get-county.py
@@ -3,7 +3,7 @@ import geocoder
 
 g = geocoder.bing( 'Mountain View, CA', key=os.environ[ 'BING_API_KEY' ] )
 
-print( g.json[ 'address' ] )
+print( g.json[ 'raw' ][ '__type' ] )
 
 
 # --- END --- #
